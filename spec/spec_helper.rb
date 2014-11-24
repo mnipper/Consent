5a542466d4891a1a3d0ce3dc84c21e99100a20a4
Michael Nipper <mjn4406@gmail.com>
2014-11-24 13:20:22 -0500

    Add database cleaner for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,4 +58,18 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
