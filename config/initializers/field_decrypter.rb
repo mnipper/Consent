f45fd6abf88dd9fa46edf4e6d93bcc80acfaeb52
Michael Nipper <mjn4406@gmail.com>
2014-11-24 16:32:12 -0500

    Only use test key for test and development

--- config/initializers/field_decrypter.rb
+++ config/initializers/field_decrypter.rb
@@ -1,1 +1,3 @@
-$field_decrypter = FieldDecrypter.new("#{Rails.root}/spec/resources/test_private_key.pem")
+$field_decrypter = if Rails.env.development? or Rails.env.test?
+  FieldDecrypter.new("#{Rails.root}/spec/resources/test_private_key.pem")
+end
