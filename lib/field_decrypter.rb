b29db7decba2b3f25b342c07b0311604cc9c5d89
Michael Nipper <mjn4406@gmail.com>
2014-11-25 13:46:48 -0500

    Add json parsing to field decrypter

--- lib/field_decrypter.rb
+++ lib/field_decrypter.rb
@@ -12,11 +12,12 @@ class FieldDecrypter
     @private_key_filepath   = private_key_filepath
     @field_delimiter        = options[:field_delimiter] || '::'
     @symmetric_algorithm    = options[:symmetric_algorithm] || 'AES-256-CBC'
+    @format                 = options[:format] || :delimited
   end
 
   def decrypt(field)
     return if field.blank?
-    iv, encrypted_aes_key, encrypted_text = field.split(field_delimiter)
+    iv, encrypted_aes_key, encrypted_text = parse(field)
     private_key = OpenSSL::PKey::RSA.new(File.read(private_key_filepath))
     decrypted_aes_key = private_key.private_decrypt(Base64.decode64(encrypted_aes_key))
     decrypt_data(iv, decrypted_aes_key, encrypted_text)
@@ -37,5 +38,15 @@ class FieldDecrypter
       aes.key = key
       aes.iv = Base64.decode64(iv)
       aes.update(Base64.decode64(encrypted_text)) + aes.final
+    end
+
+    def parse(field)
+      case @format
+      when :delimited
+        field.split(field_delimiter)
+      when :json
+        parsed = JSON.parse(field)
+        [parsed['iv'], parsed['key'], parsed['message']]
+      end
     end
 end
--- spec/lib/field_decrypter_spec.rb
+++ spec/lib/field_decrypter_spec.rb
@@ -29,4 +29,26 @@ describe FieldDecrypter do
       expect { @field_decrypter.field_delimiter = char }.to raise_error
     end
   end
+
+  describe "json parsing" do
+    before :each do
+      @field_decrypter_json = FieldDecrypter.new("#{Rails.root}/spec/resources/test_private_key.pem", format: :json)
+      @message = {}
+      @message['iv'] = 'RC78JOorP4EKyuh3Bh9atg=='
+      @message['key'] = 'iiaRmPaoFbke5d81FkL5SgQYudtnV2rl370QVlJel0f2+IniSugQ359gvsTqT2YPg1JcZ+9eky9GLGoDdAX90jsqYBG3CnawS6FgHNfO2HxBxTcgTWgfRmZv1lLo4JJ423lVXMdQv2mxgBTTMMO1ZNJsHtDjOoKlTGPh1lL3DARRrkz67J8LKN/zGDMIQXr9kfag/qkCJgJB36Owsj+9qIKKrzxtznsp/t8/Q2JGaKJmvh0srKr35hXJ6+cucI/+2uhEE78oKadUTyxxzgKrrmhKkKW2TvO4BPRsA7kpU6/X44UYYxQ2eqiMrhvklbzZKgtOuk84LZg4gXi9zCc80g=='
+      @message['message'] = 'gPYO7iDShmrk+RzO5ydjMfSUizmVEA6dAdrpsLeZvqI=' 
+    end
+
+    it "should parse a json field" do
+      iv, key, m = @field_decrypter_json.send(:parse, @message.to_json)
+      iv.should  == @message['iv']
+      key.should == @message['key']
+      m.should   == @message['message']
+    end
+
+    it "should decrypt a json value" do
+      @field_decrypter_json = FieldDecrypter.new("#{Rails.root}/spec/resources/test_private_key.pem", format: :json)
+      @field_decrypter_json.decrypt(@message.to_json).should == "Hello out there!"
+    end
+  end
 end
